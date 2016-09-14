911350b6f6ab61f5b4c8de1538f617caec8cecc3
andrew.ryan <andrew.g.m.ryan@gmail.com>
2016-09-14 11:33:30 +0100

    changed tabs to white space

--- url.js
+++ url.js
@@ -1,28 +1,28 @@
 define(["underscore"], function(_) {
-	var module = {};
+    var module = {};
 
-	/**
-	 * convert query string to object
-	 */
-	module.to_object = function(query) {
-		var result = {};
-		_.each(query.split(";"), function(fragment) {
-			name = fragment.split("=")[0];
-			value = fragment.split("=")[1];
-			result[name] = value;
-		}, this);
-		return result;
-	};
+    /**
+     * convert query string to object
+     */
+    module.to_object = function(query) {
+        var result = {};
+        _.each(query.split(";"), function(fragment) {
+            name = fragment.split("=")[0];
+            value = fragment.split("=")[1];
+            result[name] = value;
+        }, this);
+        return result;
+    };
 
-	/**
-	 * convert object to query string
-	 */
-	module.to_query = function(data) {
-		var fragments = _.map(data, function(value, key) {
-			return [key, value].join("=");
-		});
-		return fragments.join(";");
-	};
+    /**
+     * convert object to query string
+     */
+    module.to_query = function(data) {
+        var fragments = _.map(data, function(value, key) {
+            return [key, value].join("=");
+        });
+        return fragments.join(";");
+    };
 
-	return module;
+    return module;
 });
